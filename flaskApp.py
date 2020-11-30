03440160e3d895630f8af75036ac9eb18457862a
Paul Kaster <ptkaster1@gmail.com>
2020-11-29 23:23:13 -0800

    debug webhook

--- flaskApp.py
+++ flaskApp.py
@@ -27,7 +27,6 @@ def git_pull():
     except Exception as asc:
         return str(asc)
 
-
 @app.route('/static/images/<image>')
 def serve_image(image):
     return app.send_static_file('site/images/' + image)
