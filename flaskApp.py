2dd8097aaa445f038458972f8104d33c28239db0
Paul Kaster <ptkaster1@gmail.com>
2020-11-29 22:59:49 -0800

    adding github webhook v1

--- flaskApp.py
+++ flaskApp.py
@@ -17,6 +17,11 @@ def hello():
 def serve_image():
     return app.send_static_file('/temp_images/loaderio-198f3924fd75b1b73e6fc5855b70c71c.txt')
 
+@app.route('server/pullgithub')
+def serve_image():
+    os.system("git pull")
+    return ""
+
 @app.route('/static/images/<image>')
 def serve_image(image):
     return app.send_static_file('site/images/' + image)
