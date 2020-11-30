06d4801bac3f799ead4175058766856633a2bc4a
Paul Kaster <ptkaster1@gmail.com>
2020-11-29 23:25:55 -0800

    debug webhook

--- flaskApp.py
+++ flaskApp.py
@@ -21,7 +21,7 @@ def serve_loader_static():
 @app.route('/server/pullgithub')
 def git_pull():
     try:
-        return os.system("git pull")
+        return str(os.system("git pull"))
         os.system("systemctl reboot -i")
         return "Executed command"
     except Exception as asc:
